d1b63caedbbddd1747eaff67a411d4a081d15f24
Amanda Rover <rover.amanda@gmail.com>
2016-01-28 20:04:53 -0200

    Created anagram generator

--- app/controllers/anagrams_controller.rb
+++ app/controllers/anagrams_controller.rb
@@ -5,6 +5,9 @@ class AnagramsController < ApplicationController
 
 	def show
 		@anagram = Anagram.find(params[:id])
+
+		@anagramAnalysis = anagram_checker(:word1, :word2)
+
 	end
 
 	def new
@@ -13,13 +16,40 @@ class AnagramsController < ApplicationController
 	def create
 		@anagram = Anagram.new(anagram_params)
 
-		@anagram.save
-		redirect_to @anagram
+		if @anagram.save
+			redirect_to @anagram
+		else
+			render 'new'
+		end
 	end
 
+	
 	private
 	  def anagram_params
 	    params.require(:anagram).permit(:word1, :word2)
+	  end
+
+	  def anagram_checker(word1, word2)
+	  	
+	  	if @anagram.word1.length > 0 && @anagram.word2.length > 0
+ 			
+ 			if @anagram.word1.length.eql? (@anagram.word2.length)
+				@word1 = @anagram.word1.chars.sort.join
+	  			@word2 = @anagram.word2.chars.sort.join
+
+	  			if @word1.eql? (@word2)
+	  				"They are anagrams!!!!"
+	  			else
+	  				"They aren't anagrams..."
+	  			end
+
+	  		else
+	  			"They aren't anagrams...Their length is diferent"
+	  		end
+ 		else
+	  		"Fill in the two fields please"	  		
+	  	end	
+
 	  end
 
 end
