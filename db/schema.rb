ffeb846e75b65c14f15fe4a82eb763faf18d7462
Amanda Rover <rover.amanda@gmail.com>
2016-01-29 18:04:00 -0200

    Change database to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160128032339) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "anagrams", force: :cascade do |t|
     t.string   "word1"
     t.string   "word2"
